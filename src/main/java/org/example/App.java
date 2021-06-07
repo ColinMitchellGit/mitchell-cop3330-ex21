a11aea1ffb610021948e4c2178e616488895df48
Colin Mitchell <benhawk6003@gmail.com>
2021-06-06 21:38:43 -0400

    exercise 21

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,63 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Colin Mitchell
+ */
+
+package org.example;
+import java.util.Scanner;
+import static java.lang.Integer.parseInt;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the number of the month: ");
+        int month_num = parseInt(input.nextLine());
+        String month;
+
+        switch(month_num) {
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + month_num);
+        }
+
+        System.out.println("The name of the month is " + month + ".");
+    }
+}
